8e6734c8751cfc3cfd2e6db074d854226900ad6f
priyanka <priyankasaini9326@gmail.com>
2026-01-16 15:07:03 +0530

    theme add on details sections

--- src/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import styles from './MovieDetails.module.css'
+import { ThemeContext } from '../../Components/Contextapi/ThemecontextApi'
 const MovieDetails = () => {
   let { id } = useParams()
   const [movie, setmovie] = useState(null)
@@ -9,6 +10,7 @@ const MovieDetails = () => {
   const [loading, setloading] = useState(false)
   let moviekey = 'cab9c0834cb39cc84ed4d68048c9e03e'
   let navigate = useNavigate()
+  let { Theme } = useContext(ThemeContext)
 
   useEffect(() => {
     setloading(true)
@@ -16,6 +18,8 @@ const MovieDetails = () => {
       try {
         let res = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${moviekey}&language=en-US`)
         setmovie(res.data)
+        console.log(res.data);
+
         seterror('')
       } catch (error) {
         seterror('Failed to fetch movie details')
@@ -30,13 +34,15 @@ const MovieDetails = () => {
   if (!movie) return null
 
   return (
-    <div className={styles.moviedetailscontainer}>
-      <div style={{ padding: "20px" }}>
+    <div className={`${styles.moviedetailscontainer} ${Theme ? styles.light : styles.dark}`}>
+      <div style={{ padding: "20px" }} className={`${styles.cardDetails} ${Theme ? styles.cardlight : styles.carddark}`}>
         <button onClick={() => navigate(-1)}>⬅ Back</button>
-
         <h1>{movie.title}</h1>
         <h3>{movie.tagline}</h3>
+        <p><strong>Language:</strong> {movie.spoken_languages[0]?.english_name || 'N/A'}</p>
+        <p><strong>Popularity:</strong> {movie.popularity}</p>
         <p><strong>Rating:</strong> ⭐ {movie.vote_average}</p>
+        <p><strong>Status:</strong> {movie.status}</p>
         <p><strong>Release Date:</strong> {movie.release_date}</p>
         <p><strong>Runtime:</strong> {movie.runtime} mins</p>
         <p><strong>Genres:</strong> {movie.genres?.map(g => g.name).join(", ")}</p>
